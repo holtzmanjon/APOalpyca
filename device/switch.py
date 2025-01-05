b2a0cc60d7a966e6b9a17b5dddfefd4a70ff263b
Jon Holtzman <holtz@nmsu.edu>
2025-01-04 17:01:56 -0700

    debug

--- device/switch.py
+++ device/switch.py
@@ -74,6 +74,8 @@ class action:
     def on_put(self, req: Request, resp: Response, devnum: int):
         if devnum == 0 :
             idstr = get_request_field('Parameters', req)      # Raises 400 bad request if missing
+            import pdb
+            pdb.set_trace()
             try:
                 id = int(idstr)
             except:
