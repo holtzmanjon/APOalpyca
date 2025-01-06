94030f0fac37f62cc3f791f425edafed6f374b9f
Jon Holtzman <holtz@nmsu.edu>
2025-01-06 10:02:08 -0700

    change theme to see what it looks like

--- docs/conf.py
+++ docs/conf.py
@@ -37,5 +37,24 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'nature'
+#html_theme = 'nature'
 html_static_path = ['_static']
+
+# https://sphinx-themes.org/sample-sites/sphinx-rtd-theme/
+html_theme = 'sphinx_rtd_theme'
+
+#
+# https://sphinx-rtd-theme.readthedocs.io/en/stable/configuring.html
+#
+html_theme_options = {
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'navigation_depth': -1,
+    'prev_next_buttons_location': 'both',
+    'style_external_links': True,
+    'style_nav_header_background': '#000040',
+    'vcs_pageview_mode': 'edit',
+}
+
+#
+
