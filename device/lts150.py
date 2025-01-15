2836d7e69c2b9ab5de972a661abecb37408de85d
Jon Holtzman <holtz@nmsu.edu>
2025-01-15 11:01:43 -0700

    typo

--- device/lts150.py
+++ device/lts150.py
@@ -115,7 +115,7 @@ class LTS150 :
         vel_params.MaxVelocity = Decimal(50.0)  # This is a bad idea
         self.device.SetVelocityParams(vel_params)
 
-   def ismoving(self) :
+    def ismoving(self) :
         p1 = get_position()
         time.sleep(0.5)
         p2 = get_position()
