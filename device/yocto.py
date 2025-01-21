2625a4b5ab0cb9e0b93c1c3e1ce8d11cf1491cc0
Jon Holtzman <holtz@nmsu.edu>
2025-01-21 10:50:14 -0700

    import usbrelay

--- device/yocto.py
+++ device/yocto.py
@@ -2,6 +2,7 @@ from yoctopuce.yocto_api import *
 from yoctopuce.yocto_temperature import *
 from alpaca.camera import *
 from threading import Thread
+import usbrelay
 
 class Yocto :
     def __init__(self, logger=None ) :
